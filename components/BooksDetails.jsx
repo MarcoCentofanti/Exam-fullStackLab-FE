ce86d4574c7737f190654a66742bc01ac81a1a28
marcocentofanti <marcocentofanti@hotmail.it>
2026-08-24 17:11:50 +0200

    fix: hanlde optional book cover

--- components/BooksDetails.jsx
+++ components/BooksDetails.jsx
@@ -6,28 +6,30 @@ import { useNavigate } from "react-router-dom";
 export default function BookDetails() {
   const params = useParams();
   const id = params.id;
-  const [bookDetails, setbookDetails] = useState({});
+  const [bookDetails, setBookDetails] = useState({});
   const navigate = useNavigate();
 
-  const fetchbookDetails = () => {
+  const fetchBookDetails = () => {
     axios
       .get(`http://localhost:3000/api/books/${id}`)
       .then((res) => {
-        setbookDetails(res.data);
+        setBookDetails(res.data);
       })
       .catch((err) => {
         console.log(err.message);
       });
   };
 
-  useEffect(fetchbookDetails, [id]);
+  useEffect(fetchBookDetails, [id]);
 
   return (
     <>
-      <img
-        src={`http://localhost:3000/${bookDetails.image}` || "public/test.png"}
-        alt=""
-      />
+      {bookDetails.image && (
+        <img
+          src={`http://localhost:3000/${bookDetails.image}`}
+          alt={`Copertina di ${bookDetails.title}`}
+        />
+      )}
       <h2>{bookDetails.title}</h2>
       <p>{bookDetails.author}</p>
       <p>{bookDetails.publication_year}</p>
